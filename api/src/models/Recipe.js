a2a348d92111842ec09ad729699d6498cfa0294b
agudlc <87042363+agudlc@users.noreply.github.com>
2021-10-26 22:58:35 -0300

    first day crap :(

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,37 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
-    name: {
+    Nombre: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    ID: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    Resumendelplato: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Puntuación: {
+      type: DataTypes.FLOAT,
+    },
+    Niveldecomidasaludable: {
+      type: DataTypes.STRING,
+    },
+    Pasoapaso: {
+      type: DataTypes.STRING,
+    }
+  })
+  // sequelize.define("tipoDeDieta", {
+  //   ID: {
+  //     type: DataTypes.UUID,
+  //     allowNull: false,
+  //   },
+  //   Nombre: {
+  //     type: DataTypes.STRING,
+  //     allowNull: false,
+  //   },
+
+  // });
 };
